f8f4dbcd4831343405bf2767bb26312a3599200e
MicheleFranciolapilla <michele.franciolapilla@gmail.com>
2023-06-14 22:28:40 +0900

    Model, Controller, Migration e Seeder creati

---
+++ app/Http/Controllers/Project_controllers/ComicsController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Project_controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ComicsController extends Controller
+{
+    //
+}
---
+++ app/Models/Project_models/ComicsModel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Project_models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ComicsModel extends Model
+{
+    use HasFactory;
+}
